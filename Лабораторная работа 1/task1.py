3f3da9e978d1bf9b3d0bcc1e5391e6d1f3768b3d
AlekseyF3 <56193692+AlekseyF3@users.noreply.github.com>
2024-10-24 15:13:20 +0300

    Update task1.py

--- Лабораторная работа 1/task1.py
+++ Лабораторная работа 1/task1.py
@@ -1,8 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-a = sum(filter(None,numbers))
-b = len(numbers)
-aver = a/b
-numbers[4] = aver
+sum_numbers = sum(filter(None, numbers))
+length = len(numbers)
+aver = a\sum_numbers/length
+numbers[4] = average
 print("Измененный список:", numbers)
