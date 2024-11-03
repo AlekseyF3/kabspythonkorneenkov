b512f4bd26a7fa7b1f065148ea11c69d2fd3be99
AlekseyF3 <56193692+AlekseyF3@users.noreply.github.com>
2024-11-03 13:48:45 +0200

    task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -1,5 +1,3 @@
-# TODO Напишите функцию find_common_participants
-
 def find_common_participants(first, second, znak=","):
     first_set = set(first.split(znak))
     second_set = set(second.split(znak))
@@ -10,7 +8,6 @@ def find_common_participants(first, second, znak=","):
     common_participants = first_set.intersection(second_set)
     return sorted(list(common_participants))
 
-# TODO Провеьте работу функции с разделителем отличным от запятой
 
 first = "Иванов|Петров|Сидоров"
 second = "Петров|Сидоров|Смирнов"
