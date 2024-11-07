2f9d6402ddad36f4a94417b1695940dedecf7af0
AlekseyF3 <56193692+AlekseyF3@users.noreply.github.com>
2024-11-07 22:11:43 +0200

    Update task1.py

--- Лабораторная работа 3/task1.py
+++ Лабораторная работа 3/task1.py
@@ -1,13 +1,14 @@
-def find_index(items_list,find_item):
+def find_index(items_list, find_item):
     if find_item in items_list:
         return items_list.index(find_item)
     else:
         return None
 
+
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
 for find_item in ['банан', 'груша', 'персик']:
-    index_item = find_index(items_list,find_item) # TODO Вызовите функцию, что получить индекс товара
+    index_item = find_index(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
     if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
